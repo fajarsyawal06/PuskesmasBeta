a673297796b6ccfc5a17b304e68afe30edff13bd
fajarsyawal06 <muhammad.reyhann29@gmail.com>
2026-05-08 09:14:00 +0800

    tableConfig writeable

--- src/config/tableConfig.js
+++ src/config/tableConfig.js
@@ -7,34 +7,7 @@ export const barisReadonly = [
 ];
 
 export const barisRumus = [
-    "Persentase Balita yang dipantau pertumbuhan dan perkembangannya",
-    "Cakupan balita sakit dilayani menggunakan MTBS di FKTP",
-    "Lahir Hidup :",
-    "Persentase bayi baru lahir dari ibu yang positip HIV dan sifilis mendapatkan deteksi dini HIV dan sifilis dalam suatu wilayah",
-    "Persentase seluruh bayi baru lahir hidup dari ibu yang positif HIV, sifilis dan hepatitis B mendapatkan tatalaksana sesuai standar dalam suatu wilayah",
-    "Persentase bayi lahir prematur (<37 minggu)",
-    "Cakupan Bayi baru lahir dengan asfiksia neonatorum",
-    "Cakupan Bayi baru lahir dengan asifiksia neonatorum yang mendapat tata laksana",
-    "Persentase bayi baru lahir dengan berat lahir rendah mendapat tata laksana",
-    "Asuhan Neonatal 0 - 6 Jam",
-    "Cakupan KN 1",
-    "Cakupan Kunjungan Neonatal (KN) lengkap sesuai standar",
-    "Cakupan Bayi Baru Lahir Yang Dilakukan SHK",
-    "Absolut Neonatus Komplikasi",
-    "Persentase Neonatus Komplikasi",
-    "Jumlah Komplikasi pada Neonatus :",
-    "Persentase Kunjungan Bayi dalam bulan berjalan",
-    "Balita yang dipantau pertumbuhan dan perkembangannya (balita usia 0-59 bulan yang yang dipantau pertumbuhan dan perkembangan)",
-    "Persentase Balita yang dipantau pertumbuhan dan perkembangannya",
-    "Kunjungan Anak Balita  Usia 12 - 23 Bulan",
-    "- Usia 12 bulan",
-    "Kunjungan Anak Balita Usia 24 - 35 Bulan",
-    "Kunjungan Anak Balita  Usia 36 - 59 Bulan",
-    "Total Anak Balita Usia 0 - 60 Bulan (Sesuai Usia/Waktu SDIDTK)",
-    "Jumlah Kunjungan Balita Sakit di FKTP",
-    "Jumlah balita sakit dilayani menggunakan MTBS di FKTP",
-    "Balita Memiliki Buku KIA",
-    "Menajemen Terpadu Balita Sakit",
+
 ];
 
 export const barisSatuKolom = [
